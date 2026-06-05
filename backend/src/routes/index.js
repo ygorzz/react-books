227ddf4234da5b866bb3448a56c22c4ccb10021b
Ygor Santos <santosygor2007@gmail.com>
2026-06-05 11:39:01 -0300

    feat: add error handling

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import livrosRoutes from "./livrosRoutes.js";
+import manipuladorErros from '../middlewares/manipuladorErros.js';
 
 function routes(app){
     app.use(express.json());
     app.use(livrosRoutes);
+    app.use(manipuladorErros)
 }
 
 export default routes;
