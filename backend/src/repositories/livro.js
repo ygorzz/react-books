c02a9e4740d178eb7ee3876070ad02658e6d5f5a
Ygor Santos <santosygor2007@gmail.com>
2026-06-05 11:35:25 -0300

    refactor: separate business logic and data persistence

---
+++ backend/src/repositories/livro.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+import path from "path";
+
+// Torna caminho compatível com diferentes SOs 
+const filePath = path.resolve("./livros.json");
+
+async function findAll() {
+    const livros = await fs.promises.readFile(filePath, "utf-8");
+    return JSON.parse(livros);
+}
+
+async function save(livro) {
+    await fs.promises.writeFile(filePath, JSON.stringify(livro, null, 2));
+}
+
+export { findAll, save }  
