b048d913e6ed584d1578386f2c4f962ca03e97b5
Ygor Santos <santosygor2007@gmail.com>
2026-06-07 00:06:40 -0300

    fix: configure CORS policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors({origin: "*"})); // Configura o CORS / * -> permite acesso para qualquer endereço
 routes(app);
 
 export default app;
