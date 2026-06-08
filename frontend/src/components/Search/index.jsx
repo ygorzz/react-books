059bec6be3063b18429ecff081fb7e33b093935f
Ygor Santos <santosygor2007@gmail.com>
2026-06-08 10:40:00 -0300

    feat: add favoritos service in frontend

--- frontend/src/components/Search/index.jsx
+++ frontend/src/components/Search/index.jsx
@@ -1,14 +1,15 @@
 import Input from "../Input/index.js";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
-import { getLivros } from "../../services/livros.js";
+import { getLivros } from "../../services/livrosService.js";
+import { postFavoritos } from "../../services/favoritosService.js";
 
 const SearchContainer = styled.section`
   text-align: center;
   color: #fff;
   padding: 85px 0;
   height: 470px;
-  width: 100%;  
+  width: 100%;
 `;
 
 const Titulo = styled.h2`
@@ -23,22 +24,21 @@ const Subtitulo = styled.h3`
 `;
 
 const Resultado = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 20px;
-    cursor: pointer;
-    p {
-        width: 200px;
-    }
-    img {
-        width: 100px;
-    }
-    &:hover {
-        border: 1px solid white;
-    }
-`
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+    width: 200px;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
 
 function Search() {
   // Lógica de estado para acompanhar e renderizar automaticamente o valor da busca na interface
@@ -56,9 +56,14 @@ function Search() {
   }, []);
 
   // Essa função é criada pois useEffect não aceita o uso do async/await
-  async function fetchLivros(){
-    const livrosDaApi = await getLivros()
-    setLivros(livrosDaApi); 
+  async function fetchLivros() {
+    const livrosDaApi = await getLivros();
+    setLivros(livrosDaApi);
+  }
+
+  async function insertFavorito(id) {
+    await postFavoritos(id);
+    alert('Livro adicionado como favorito!');
   }
 
   return (
@@ -70,17 +75,22 @@ function Search() {
         placeholder="Procure aqui sua leitura"
         // Pega o valor do elemento (target) que disparou o evento, atualiza e renderiza na interface
         onChange={(evento) => {
-          const resultado = livros.filter((livro) =>
-            livro.titulo.includes(evento.target.value),
-          );
-          setLivrosPesquisados(resultado);
+          const search = evento.target.value;
+          if (search && search !== " ") {
+            const resultado = livros.filter((livro) =>
+              livro.titulo.includes(search),
+            );
+            setLivrosPesquisados(resultado);
+          } else {
+            setLivrosPesquisados([]);
+          }
         }}
       />
 
       {livrosPesquisados.map((livro) => {
         return (
-          <Resultado>
-                <p>{livro.titulo}</p>
+          <Resultado onClick={() => insertFavorito(livro.id)}>
+            <p>{livro.titulo}</p>
           </Resultado>
         );
       })}
--- frontend/src/routes/Favoritos.jsx
+++ frontend/src/routes/Favoritos.jsx
@@ -1,5 +1,6 @@
-import Search from "../components/Search/index.jsx";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { deleteFavoritos, getFavoritos } from "../services/favoritosService.js";
 
 const FavoritosContainer = styled.div`
   width: 100vw;
@@ -7,12 +8,72 @@ const FavoritosContainer = styled.div`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
 `;
 
-// Componente React -> função que retorna um JSX
-// Favoritos -> componente pai
+const ResultadoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+  cursor: pointer;
+  text-align: center;
+  padding: 0 100px;
+  p {
+    width: 200px;
+    color: #fff;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
+const Titulo = styled.h2`
+  color: #fff;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+  padding-top: 35px;
+`;
+
 function Favoritos() {
+  const [favoritos, setLivrosFavoritos] = useState([]);
+
+  useEffect(() => {
+    fetchFavoritos();
+  }, []);
+
+  async function fetchFavoritos() {
+    const favoritosDaApi = await getFavoritos();
+    setLivrosFavoritos(favoritosDaApi);
+  }
+
+  async function deleteFavorito(id){
+    await deleteFavoritos(id);
+    alert('Livro removido de favoritos!');
+    fetchFavoritos();
+  }
+
   return (
     <FavoritosContainer>
-      <Search />
+      <div>
+        <Titulo>Aqui estão seus livros favoritos:</Titulo>
+        <ResultadoContainer>
+          <Resultado>
+            {
+              favoritos.message ? <p>{favoritos.message}</p> :  
+                favoritos.map((favorito) => (
+                  <p onClick={() => deleteFavorito(favorito.id)}>
+                    {favorito.titulo}
+                  </p>
+                ))
+            }
+          </Resultado>
+        </ResultadoContainer>
+      </div>
     </FavoritosContainer>
   );
 }
